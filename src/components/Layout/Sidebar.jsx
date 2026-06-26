8ce090049685ae5ae3f5af68dabee0c4a307fc93
dev-mahata <debtronyx@gmail.com>
2026-06-26 23:36:57 +0530

    Initial project setup

---
+++ src/components/Layout/Sidebar.jsx
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+import logo from "../../assets/logo/logo.png";
+import {
+  BarChart3,
+  ChevronDown,
+  LayoutDashboard,
+  Settings,
+  Users,
+} from "lucide-react";
+
+const menuItems = [
+  {
+    id: "dashboard",
+    icon: LayoutDashboard,
+    label: "Dashboard",
+    active: true,
+    badge: "New",
+  },
+  {
+    id: "analytics",
+    icon: BarChart3,
+    label: "Analytics",
+    submenu: [
+      { id: "overview", label: "Overview" },
+      { id: "reports", label: "Reports" },
+      { id: "insights", label: "Insights" },
+    ],
+  },
+  {
+    id: "users",
+    icon: Users,
+    label: "Users",
+    count: "2.4k",
+    submenu: [
+      { id: "all-users", label: "All Users" },
+      { id: "roles", label: "User Activity" },
+    ],
+  },
+  {
+    id: "settings",
+    icon: Settings,
+    label: "Settings",
+  },
+];
+
+function Sidebar({
+  collapsed,
+  onToggle,
+  currentPage,
+  onPageChange,
+}) {
+  const [expandedItems, setExpandedItems] = useState(
+    new Set(["analytics"]),
+  );
+
+  const toggle = (itemId) => {
+    const newExpanded = new Set(expandedItems)
+  };
+  return (
+    <div
+      className={`${collapsed ? "w-20" : "w-64"} transition duration-300 ease-in-out bg-white/80 
+      dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-200/50 
+      dark:border-slate-700/50 flex flex-col relative z-10`}
+    >
+      <div className="p-6 border-b border-slate-200/50 dark:border-slate-700/50">
+        <div className="flex items-center space-x-3">
+          <div className="">
+            <img src={logo} className="h-10 w-10" alt="" />
+          </div>
+
+          {!collapsed && (
+            <div className="">
+              <h1 className="text-xl font-bold text-slate-800 dark:text-white">
+                Crecox
+              </h1>
+              <p className="text-xs text-slate-500 dark:text-slate-400">
+                Admin Panel
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* Navigation */}
+      <nav className="flex-1 p-4 space-y-2 overflow-y-auto">
+        {menuItems.map((item) => {
+          return (
+            <div key={item.id}>
+              <button
+                className={`w-full flex items-center justify-between p-3 rounded-xl
+              transition-all duration-200 ${
+                currentPage === item.id || item.active
+                  ? "bg-gradient-to-r from-blue-500 to-purple-600 text-white shadow-lg shadow-blue-500/25"
+                  : "text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800/50"
+              }`}
+              >
+                <div className="flex items-center space-x-3">
+                  <item.icon className={`w-5 h-5`} />
+                  {/* Conditional Rendering */}
+                  <>
+                    {!collapsed && (
+                      <span className="font-medium ml-2">
+                        {item.label}
+                      </span>
+                    )}
+                    {item.badge && (
+                      <span className="px-2 py-1 text-xs bg-red-500 text-white rounded-full">
+                        {item.badge}
+                      </span>
+                    )}
+                    {item.count && (
+                      <span className="px-2 py-1 text-xs bg-slate-200 dark:bg-slate-700 text-slate-600 dark:text-slate-300 rounded-full">
+                        {item.count}
+                      </span>
+                    )}
+                  </>
+                </div>
+
+                {!collapsed && item.submenu && (
+                  <ChevronDown
+                    className={`w-4 h-4 transition-transform`}
+                  />
+                )}
+              </button>
+
+              {/* Sub Menus  */}
+              {!collapsed && item.submenu && (
+                <div className="ml-8 mt-2 space-y-1">
+                  {item.submenu.map((subitem) => {
+                    return (
+                      <button key={subitem.id}>
+                        {subitem.label}
+                      </button>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </nav>
+
+      {/* User Profile */}
+      {!collapsed && (
+        <div className="p-4 border-t border-slate-200/50 dark:border-slate-700/50">
+          <div
+            className="flex items-center space-x-3 p-3 rounded-xl bg-slate-50
+        dark:bg-slate-800./50"
+          >
+            <img
+              src="https://images.pexels.com/photos/34099725/pexels-photo-34099725.jpeg?
+            _gl=1*1hmlx6*_ga*MjA4NDg0NjU4MS4xNzgyNDc2OTMx*_ga_8JE65Q40S6*czE3ODI0NzY5M
+            zAkbzEkZzEkdDE3ODI0NzcwMzgkajE2JGwwJGgw"
+              alt="user"
+              className="w-10 h-10 rounded-full ring-2 ring-blue-500"
+            />
+            <div className="flex-1 min-w-0">
+              <div className="flex-1 min-w-0">
+                <p className="text-sm font-medium text-slate-800 dark:text-white truncate">
+                  Debnath Mahata
+                </p>
+                <p className="text-xs text-slate-500 dark:text-slate-400 truncate">
+                  Administrator
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Sidebar;
